cd2b5058b3bbf984b1513abc41deaa3a4d197a78
R <108593051+rafaelorrego@users.noreply.github.com>
2024-04-08 22:05:23 -0400

    mi portafolio

---
+++ imagenes/desafio.js
@@ -0,0 +1,16 @@
+// ingresar 3 numneroas y comparar cvual es mayor 
+ 
+ 
+let  num1 = 8 ;
+let num2 = 5; 
+let num3 = 5;
+ 
+
+ if (num1>num2 && num1>num3)  n{
+    console.log (" el mayor es el numero 1 ")
+
+ }if (num2 > num1 & num2 > num3){
+
+    console.log(" el mayor es el numero 2")
+
+ } if (  )
